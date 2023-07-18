d093294242caeea956ca2215ed7127d620e87335
samumaida <115372139+samumaida@users.noreply.github.com>
2023-07-18 17:51:04 +0200

    add icons currency

--- src/app/currency-converter/currency-converter.component.ts
+++ src/app/currency-converter/currency-converter.component.ts
@@ -14,10 +14,9 @@ export class CurrencyConverterComponent {
   @ViewChild("option")option!: any
 
   valute: Valute[] = [
-    { value: 'euro', viewValue: 'EUR - €'},
-    { value: 'swiss-franc', viewValue: 'CHF - SWISS FRANC'},
-    { value: 'dollar', viewValue: 'DOLLAR - $'},
-    { value: 'sterlin-3', viewValue: 'STERLIN - £'}
+    { value: 'swiss-franc', viewValue: '🇨🇭 CHF (swiss-franc)' },
+    { value: 'dollar', viewValue: '🇺🇸 $ DOLLAR' },
+    { value: 'sterlin-3', viewValue: '🇬🇧 £ STERLIN' },
   ];
 
   decimal: any = [];
@@ -27,7 +26,7 @@ simbol = ''
 onKeyUp(x:any) {
   const r = document.getElementsByClassName('mat-mdc-select-min-line')
   console.log(r)
-  if(r[0].innerHTML === 'CHF - SWISS FRANC'){
+  if(r[0].innerHTML === '🇨🇭 CHF (swiss-franc)'){
     this.currencyValue = ''
     this.simbol = ' CHF'
    let n = this.currencyValue += (x.target.value * 0.96).toFixed(2);
@@ -35,12 +34,12 @@ onKeyUp(x:any) {
    const f = this.decimal
    console.log(f)
   }
-  else if(r[0].innerHTML == 'DOLLAR - $'){
+  else if(r[0].innerHTML == '🇺🇸 $ DOLLAR'){
     this.currencyValue = ''
     this.simbol = ' $'
     this.currencyValue += (x.target.value * 1.12).toFixed(2);
   }
-  else if(r[0].innerHTML == 'STERLIN - £'){
+  else if(r[0].innerHTML == '🇬🇧 £ STERLIN'){
     this.currencyValue = ''
     this.simbol = ' £'
     this.currencyValue += (x.target.value * 0.85).toFixed(2);
